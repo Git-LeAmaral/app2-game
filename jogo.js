ffceb93f6a6d31504c2022c9a3debd1c739c18a0
Leandro Amaral <lramaral02m@gmail.com>
2022-05-23 19:37:33 -0300

    Definindo dimensções do palco do jogo

---
+++ jogo.js
@@ -0,0 +1,14 @@
+
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo(){
+	 altura = window.innerHeight 
+     largura = window.innerWidth
+
+     console.log(altura, largura)
+}
+	 
+ajustaTamanhoPalcoJogo()
+
+
