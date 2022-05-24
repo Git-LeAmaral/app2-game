d03f65097d594662256b6391aa3797f17da1d122
Leandro Amaral <lramaral02m@gmail.com>
2022-05-24 12:27:29 -0300

    Adicionando cronometro e pagina vitoria

--- jogo.js
+++ jogo.js
@@ -2,6 +2,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 10
 
 function ajustaTamanhoPalcoJogo(){
 	 altura = window.innerHeight 
@@ -11,6 +12,20 @@ function ajustaTamanhoPalcoJogo(){
 }
 	 
 ajustaTamanhoPalcoJogo()
+
+var cronometro = setInterval(function() {
+
+	tempo -= 1
+
+	if(tempo < 0) {
+		clearInterval(cronometro)
+		clearInterval(criaMosca)
+		window.location.href = 'vitoria.html'
+	} else {
+		document.getElementById('cronometro').innerHTML = tempo
+	}
+		
+}, 1000)
 
 function posicaoRandomica(){
 
