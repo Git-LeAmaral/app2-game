bcbe4665daf2df4b7cdd2d14ca146533a41c9fb6
Leandro Amaral <lramaral02m@gmail.com>
2022-05-23 22:55:07 -0300

    Posições randomicas

--- jogo.js
+++ jogo.js
@@ -6,9 +6,28 @@ function ajustaTamanhoPalcoJogo(){
 	 altura = window.innerHeight 
      largura = window.innerWidth
 
-     console.log(altura, largura)
+     console.log(largura, altura)
 }
 	 
 ajustaTamanhoPalcoJogo()
 
+function posicaoRandomica(){
 
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
+
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	console.log(posicaoX, posicaoY)
+
+	//criar o elemento html
+	var mosca = document.createElement('img')
+	mosca.src = 'imagens/mosca.png'
+	mosca.className = 'mosca1'
+	mosca.style.left = posicaoX + 'px'
+	mosca.style.top = posicaoY + 'px'
+	mosca.style.position = 'absolute'
+
+	document.body.appendChild(mosca)
+}	
